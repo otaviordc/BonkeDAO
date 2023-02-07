29e1bf216148c65d62978ada208c58c5f08d177b
otaviordc <otavioribeirodecastro@hotmail.com>
2023-02-06 22:27:32 -0300

    third commit

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Container from "./Container";
 
 import styles from "./Navbar.module.css";
-import logo from "../../img/costs_logo.png";
+
 
 function Navbar() {
   return (
